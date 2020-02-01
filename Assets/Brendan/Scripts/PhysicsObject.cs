326a63d3c64a22f36edd1e34d13883afa528a5f1
Brendan Robinson <brendr1@ad.umbc.edu>
2020-02-01 11:58:54 -0500

    VR Controller

---
+++ Assets/Brendan/Scripts/PhysicsObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private Transform snapTarget;
+    private Vector3 prevPosition;
+    private Vector3 velocity;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (snapTarget)
+        {
+            transform.position = Vector3.Lerp(transform.position, snapTarget.position + snapTarget.forward / 2f, 8 * Time.fixedDeltaTime);
+            transform.rotation = snapTarget.rotation;
+        }
+        velocity = (transform.position - prevPosition) / Time.fixedDeltaTime;
+
+        prevPosition = transform.position;
+
+
+    }
+
+    public void Grab(Transform grabber)
+    {
+        snapTarget = grabber;
+        rb.useGravity = false;
+        rb.isKinematic = true;
+    }
+
+    public void Drop()
+    {
+        snapTarget = null;
+        rb.useGravity = true;
+        rb.isKinematic = false;
+        rb.velocity = velocity;
+    }
+}
