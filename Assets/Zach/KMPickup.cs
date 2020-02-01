35bee0c4dedd15295540b2a4a1f00dd775a86962
Zach <zachchale@gmail.com>
2020-02-01 13:50:27 -0500

    Can rotate, Hands animate

--- Assets/Zach/KMPickup.cs
+++ Assets/Zach/KMPickup.cs
@@ -9,6 +9,11 @@ public class KMPickup : MonoBehaviour
     private Transform grabPoint;
     private Transform camOrigin;
     public float grabForce = 10f;
+    public float rotateForce = 1f;
+    [SerializeField]
+    Animator leftHandAnim;
+    [SerializeField]
+    Animator rightHandAnim;
     private void Update() {
         GrabInput();
         RotateInput();
@@ -28,6 +33,7 @@ public class KMPickup : MonoBehaviour
             {
                 print(currentItem);
                 if(hit.transform.GetComponent<Rigidbody>()){
+                    rightHandAnim.SetBool("Grabbed", true);
                     currentItem = hit.transform;
                     currentItem.GetComponent<Rigidbody>().useGravity = false;
                     currentItem.GetComponent<Rigidbody>().drag = 5f;
@@ -37,13 +43,28 @@ public class KMPickup : MonoBehaviour
             currentItem.GetComponent<Rigidbody>().useGravity = true;
             currentItem.GetComponent<Rigidbody>().drag = 0f;
             currentItem = null;
+            rightHandAnim.SetBool("Grabbed", false);
         }
     }
     void RotateInput(){
+        if(currentItem == null)
+            return;
         if(Input.GetKeyDown(KeyCode.Mouse1)||Input.GetKeyUp(KeyCode.Mouse1)){
             if(currentItem){
                 currentItem.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
             }
+        }
+        if(Input.GetKey(KeyCode.Mouse1)){
+            float horizontal = Input.GetAxisRaw("Mouse X");
+            float vertical = Input.GetAxisRaw("Mouse Y");
+            Vector3 torque = new Vector3(0,-horizontal * rotateForce,0);
+            torque += vertical * camOrigin.right*rotateForce;
+            currentItem.GetComponent<Rigidbody>().AddTorque(torque);
+            leftHandAnim.SetBool("Rotating", true);
+            rightHandAnim.SetBool("Rotating", true);
+        }else{
+            leftHandAnim.SetBool("Rotating", false);
+            rightHandAnim.SetBool("Rotating", false);
         }
     }
     void MoveItem(){
