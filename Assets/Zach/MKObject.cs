0c457b7e4bb50c21b0070af7873b999617c2eea6
Zach <zachchale@gmail.com>
2020-02-01 17:54:52 -0500

    Nailing Stuff

---
+++ Assets/Zach/MKObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MKObject : MonoBehaviour
+{
+    bool touching = false;
+
+
+    private void OnCollisionExit() {
+        touching = false;
+    }
+ 
+    private void OnCollisionStay(Collision other){
+        if(other.transform.tag != "Object")
+            touching = true;
+    }
+    public bool isTouching(){
+        return touching;
+    }
+}
