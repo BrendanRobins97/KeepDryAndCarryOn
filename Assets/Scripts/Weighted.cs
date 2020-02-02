66a3a9bd84bd9452249e2ce8660db868e9a08639
ssandif <ssandifer@umbc.edu>
2020-02-02 14:38:16 -0500

    Skyboxes and working lamps

---
+++ Assets/Scripts/Weighted.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+interface IWeighted<T>
+{
+    T Item { get; }
+    int Weight { get; }
+}
+
+[Serializable]
+struct WeightedFurnitureType : IWeighted<FurnitureType>
+{
+    [SerializeField]
+    private FurnitureType item;
+
+    [SerializeField]
+    private int weight;
+
+    public FurnitureType Item => item;
+    public int Weight => weight;
+}
+
+[Serializable]
+struct WeightedSpawnableFurniture : IWeighted<SpawnableFurniture>
+{
+    [SerializeField]
+    private SpawnableFurniture item;
+
+    [SerializeField]
+    private int weight;
+
+    public SpawnableFurniture Item => item;
+    public int Weight => weight;
+}
+
+[Serializable]
+struct WeightedMaterial : IWeighted<Material>
+{
+    [SerializeField]
+    private Material item;
+
+    [SerializeField]
+    private int weight;
+
+    public Material Item => item;
+    public int Weight => weight;
+}
---
+++ Assets/Skyboxes/RandomSkybox.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using static WeightedProbability;
+
+public class RandomSkybox : MonoBehaviour
+{
+    [SerializeField]
+    private WeightedMaterial[] skyboxes;
+
+    private int total;
+
+    void Awake()
+    {
+        total = TotalWeights<WeightedMaterial, Material>(skyboxes);
+    }
+
+    void Start()
+    {
+        RenderSettings.skybox = skyboxes[RandomIndex<WeightedMaterial, Material>(skyboxes, total)].Item;
+    }
+}
